4d167935c8d6d1aaca89af7a8ab1eaf96eddda04
tejaswini <tejaswini_grandhe@persistent.co.in>
2015-03-19 09:46:07 +0000

    Change in file2

--- travis_test2.py
+++ travis_test2.py
@@ -1,3 +1,3 @@
 print("-----------------------------------")
-print("Hello its a python file from mysql repo of tejaswiniG account")
+print("Hello its a python file from mysql repo of tejaswiniG account test2 file")
 print("--------------EOF------------------")
